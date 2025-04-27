fa0068b893c32430d28ef30232c3daea115c4738
root <mrrahbarnia@example.com>
2025-04-27 05:16:58 -0400

    Added some tables for dynamically handling roles and permissions.

--- src/auth/v1/models.py
+++ src/auth/v1/models.py
@@ -39,3 +39,89 @@ class UserIdentity(Base):
     user_id: so.Mapped[types.UserId] = so.mapped_column(
         sa.ForeignKey(f"{User.__tablename__}.id", ondelete="CASCADE"), unique=True
     )
+
+
+class Seller(Base):
+    __tablename__ = "sellers"
+    __table_args__ = (sa.PrimaryKeyConstraint("id"),)
+    id: so.Mapped[types.SellerId] = so.mapped_column(autoincrement=True)
+    company_name: so.Mapped[str] = so.mapped_column(sa.String(200))
+    user_id: so.Mapped[types.UserId] = so.mapped_column(
+        sa.ForeignKey(f"{User.__tablename__}.id", ondelete="CASCADE")
+    )
+    is_founder: so.Mapped[bool] = so.mapped_column(default=False)
+
+
+class Role(Base):
+    # Only sellers can create or delete roles
+    __tablename__ = "roles"
+    __table_args__ = (
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint(
+            "seller_id", "name", name="uq_role_seller_name"
+        ),  # Because "Manager" role in Seller A shouldn't conflict with "Manager" role in Seller B.
+    )
+    id: so.Mapped[types.RoleId] = so.mapped_column(autoincrement=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(200))
+    description: so.Mapped[str] = so.mapped_column(sa.Text)
+    seller_id: so.Mapped[types.SellerId] = so.mapped_column(
+        sa.ForeignKey(f"{Seller.__tablename__}.id", ondelete="CASCADE"), index=True
+    )
+    created_at: so.Mapped[datetime] = so.mapped_column(server_default=sa.func.now())
+    updated_at: so.Mapped[datetime] = so.mapped_column(
+        server_default=sa.func.now(), onupdate=sa.func.now()
+    )
+
+
+class SellerStaff(Base):
+    __tablename__ = "seller_staff"
+    __table_args__ = (sa.PrimaryKeyConstraint("id"),)
+    id: so.Mapped[types.SellerStaffId] = so.mapped_column(autoincrement=True)
+    user_id: so.Mapped[types.UserId] = so.mapped_column(
+        sa.ForeignKey(f"{User.__tablename__}.id", ondelete="CASCADE"), index=True
+    )
+    seller_id: so.Mapped[types.SellerId] = so.mapped_column(
+        sa.ForeignKey(f"{Seller.__tablename__}.id", ondelete="CASCADE"), index=True
+    )
+    role_id: so.Mapped[types.RoleId] = so.mapped_column(
+        sa.ForeignKey(f"{Role.__tablename__}.id", ondelete="SET NULL"), nullable=True
+    )
+    created_at: so.Mapped[datetime] = so.mapped_column(server_default=sa.func.now())
+
+
+class Permission(Base):
+    # Only admins can create or delete permissions
+    __tablename__ = "permissions"
+    __table_args__ = (
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("name", name="uq_permission_name"),
+    )
+    id: so.Mapped[types.PermissionId] = so.mapped_column(autoincrement=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(200))
+    description: so.Mapped[str] = so.mapped_column(sa.Text)
+    created_at: so.Mapped[datetime] = so.mapped_column(server_default=sa.func.now())
+    updated_at: so.Mapped[datetime] = so.mapped_column(
+        server_default=sa.func.now(), onupdate=sa.func.now()
+    )
+
+
+class RolePermission(Base):
+    __tablename__ = "role_permissions"
+    __table_args__ = (sa.PrimaryKeyConstraint("role_id", "permission_id"),)
+    role_id: so.Mapped[types.RoleId] = so.mapped_column(
+        sa.ForeignKey(f"{Role.__tablename__}.id", ondelete="CASCADE"), index=True
+    )
+    permission_id: so.Mapped[types.PermissionId] = so.mapped_column(
+        sa.ForeignKey(f"{Permission.__tablename__}.id", ondelete="CASCADE"), index=True
+    )
+
+
+class StaffPermission(Base):
+    __tablename__ = "staff_permissions"
+    __table_args__ = (sa.PrimaryKeyConstraint("staff_id", "permission_id"),)
+    staff_id: so.Mapped[types.SellerStaffId] = so.mapped_column(
+        sa.ForeignKey(f"{SellerStaff.__tablename__}.id", ondelete="CASCADE"), index=True
+    )
+    permission_id: so.Mapped[types.PermissionId] = so.mapped_column(
+        sa.ForeignKey(f"{Permission.__tablename__}.id", ondelete="CASCADE"), index=True
+    )
--- src/auth/v1/types.py
+++ src/auth/v1/types.py
@@ -4,6 +4,10 @@ from enum import StrEnum, auto
 
 UserId = NewType("UserId", UUID)
 UserIdentityId = NewType("UserIdentityId", int)
+SellerId = NewType("SellerId", int)
+SellerStaffId = NewType("SellerStaffId", int)
+RoleId = NewType("RoleId", int)
+PermissionId = NewType("PermissionId", int)
 
 
 class UserRole(StrEnum):
